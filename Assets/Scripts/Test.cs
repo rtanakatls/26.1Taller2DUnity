ba6d1773219b15f5eed20f8c76917ec7a50c72ec
Ricardo Tanaka <rtanaka@talento.tls.edu.pe>
2026-04-29 19:28:30 -0500

    Created variable

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    private float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
